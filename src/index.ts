f0a974e7471af159d3cdb982c0db73ad8482fb71
ArminBlaze <box_for_spam@mail.ru>
2023-11-24 17:27:03 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import sum from './modules/sum';
 
 const root = document.querySelector('#root');
-root.textContent = sum(6, -1).toString();
+if(root) {
+	root.textContent = sum(6, -1).toString();
+}

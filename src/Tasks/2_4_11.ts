06ebd7bebd45274cc765e6f6902148269bd60ab9
ArminBlaze <box_for_spam@mail.ru>
2023-11-24 17:27:08 +0300

    Update 2_4_11.ts

--- src/Tasks/2_4_11.ts
+++ src/Tasks/2_4_11.ts
@@ -1,19 +1,19 @@
 function namespace(str: string): {} {
-	const regexp = /^\w\.\w(\.\w)*$/i
+	const regexp = /^\w\.\w(\.\w)*$/i;
 	if(!regexp.test(str)) {
-		throw new Error('Строка в неправильном формате')
+		throw new Error('Строка в неправильном формате');
 	}
 
-	let result = {}
+	let result = {};
 
 	str.split('.').reverse().forEach(item => {
 		result = {
-			[item]: result
-		}
-	})
-	
+			[item]: result,
+		};
+	});
+
 	return result;
 }
 
-const result = namespace('a.b.c.d.e')
+const result = namespace('a.b.c.d.e');
 console.log(result);

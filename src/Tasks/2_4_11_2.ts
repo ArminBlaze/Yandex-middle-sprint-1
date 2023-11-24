4b89ad389c669926a30951b08f242ab715cb103d
ArminBlaze <box_for_spam@mail.ru>
2023-11-24 17:27:10 +0300

    Update 2_4_11_2.ts

--- src/Tasks/2_4_11_2.ts
+++ src/Tasks/2_4_11_2.ts
@@ -1,24 +1,23 @@
-
 const input: HTMLInputElement = document.getElementById('text') as HTMLInputElement;
 const output = document.querySelector('.output') as HTMLDivElement;
 
 // версия без объекта - нафига он
 // input.addEventListener('input', (e: Event) => {
 // 	console.log(e);
-	
+
 // 	output.innerHTML = (e as any).target.value;
 // })
 
 // переделка на объект
 const data = {
-	text: ''
-}
+	text: '',
+};
 
 input.addEventListener('input', (e: Event) => {
 	const event = e as any;
 
 	data.text = event.target.value;
-	
+
 	output.innerHTML = data.text;
-	console.log(data.text)
-})
+	console.log(data.text);
+});

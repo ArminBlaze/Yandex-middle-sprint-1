3fcbfd2d55c176b7b240e85013666c9e214d2959
ArminBlaze <box_for_spam@mail.ru>
2024-01-17 21:26:58 +0300

    Create xss_test4.js

---
+++ src/Tasks/xss_test4.js
@@ -0,0 +1,21 @@
+// const str = `<script>
+// 	alert('Ataka!');
+// 	console.log('ataka!');
+// </script>`;
+// const str3 = "<img src='bad' onerror=`alert(ТВОЙ КОМПЬЮТЕР ВЗЛОМАЛИ!!!)`>";
+
+const str5 = `<SCRIPT type="text/javascript">
+	alert('Ataka!');
+</SCRIPT>`
+
+const str4 = `
+	Комментарий.
+	<img src='bad' onerror="alert('ТВОЙ КОМПЬЮТЕР ВЗЛОМАЛИ!!!')">
+	<script>
+		const peka = 'LOLKA'
+	</script>
+	<img src='bad' onerror=str5>
+`;
+
+
+console.log('подключен файл');
